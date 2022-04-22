c558d6b6c625c0bba88ecbd80f2845998ce4924a
NgocTH00 <tranhongngoc0311@gmail.com>
2022-04-22 14:12:24 +0700

    Thêm persistent

---
+++ src/Services/Balance/Balance.Persistent/Program.cs
@@ -0,0 +1,10 @@
+using Balance.Persistent;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ src/Services/Ordering/Ordering.Persistent/Program.cs
@@ -0,0 +1,10 @@
+using Ordering.Persistent;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
